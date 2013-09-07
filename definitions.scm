83247561283ac46020795222d5ee3ace56778233
David Bochenski <david@bochenski.co.uk>
2013-09-07 17:04:56 +0100

    Completes chapter 3 definitions

--- definitions.scm
+++ definitions.scm
@@ -77,5 +77,21 @@
     (cond
       ((null? lat) (quote ()))
       (else (cond 
-        ((eq? (car lat) old) (cons old (cons new (multiInsertR new old (cdr lat)))))
-        (else (cons old (multiInsertR new old (cdr lat)))))))))
+        ((eq? (car lat) old) (cons old (cons new (multiinsertR new old (cdr lat)))))
+        (else (cons (car lat) (multiinsertR new old (cdr lat)))))))))
+
+(define multiinsertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+        ((eq? (car lat) old) (cons new (cons old (multiinsertL new old (cdr lat)))))
+        (else (cons (car lat) (multiinsertL new old (cdr lat)))))))))
+
+(define multisubst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cond
+        ((eq? (car lat) old) (cons new (multisubst new old (cdr lat))))
+        (else (cons (car lat) (multisubst new old (cdr lat)))))))))
