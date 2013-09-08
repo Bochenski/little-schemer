75766b3dac8c22ae42f7b05f1abf7fdc9920c3fc
David Bochenski <david@bochenski.co.uk>
2013-09-08 17:54:14 +0100

    Ham & Cheese on Rye time
    with mustard

--- definitions.scm
+++ definitions.scm
@@ -123,8 +123,48 @@
       ((null? tup) 0)
       (else (o+ (car tup) (addtup (cdr tup)))))))
 
-(define x
+(define o*
   (lambda (n m)
     (cond
       ((zero? m) 0)
-      (else (o+ n (x n (sub1 m)))))))
+      (else (o+ n (o* n (sub1 m)))))))
+
+(define tup+
+  (lambda (tup1 tup2)
+    (cond
+      ((null? tup1) tup2)
+      ((null? tup2) tup1)
+      (else (cons (o+ (car tup1) (car tup2)) (tup+ (cdr tup1) (cdr tup2)))))))
+
+(define o>
+  (lambda (n m)
+    (cond
+      ((zero? n) #f)
+      ((zero? m) #t)
+      (else (o> (sub1 n) (sub1 m))))))
+
+(define o<
+  (lambda (n m)
+    (cond
+      ((zero? m) #f)
+      ((zero? n) #t)
+      (else (o< (sub1 n) (sub1 m))))))
+
+(define o=
+  (lambda (n m)
+    (cond
+      ((zero? m) (zero? n))
+      ((zero? n) #f)
+      (else (o= (sub1 n) (sub1 m))))))
+
+(define o^
+  (lambda (n m)
+    (cond
+      ((zero? m) 1)
+      (else (o* n (o^ n (sub1 m)))))))
+
+(define o/
+  (lambda (n m)
+    (cond
+      ((o< n m) 0)
+      (else (add1 (o/ (o- n m) m))))))
