4a76f99614dec39648d15cc934352fca8cd94974
David Bochenski <david@bochenski.co.uk>
2013-09-08 17:22:31 +0100

    addition and multiplication functions from chapter 4

--- definitions.scm
+++ definitions.scm
@@ -95,3 +95,36 @@
       (else (cond
         ((eq? (car lat) old) (cons new (multisubst new old (cdr lat))))
         (else (cons (car lat) (multisubst new old (cdr lat)))))))))
+
+; Chapter 4
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(define o+
+  (lambda (n m)
+    (cond
+      ((zero? m) n)
+      (else (add1 (o+ n (sub1 m)))))))
+
+(define o-
+  (lambda (n m)
+    (cond 
+      ((zero? m) n)
+      (else (sub1 (o- n (sub1 m)))))))
+
+(define addtup
+  (lambda (tup)
+    (cond
+      ((null? tup) 0)
+      (else (o+ (car tup) (addtup (cdr tup)))))))
+
+(define x
+  (lambda (n m)
+    (cond
+      ((zero? m) 0)
+      (else (o+ n (x n (sub1 m)))))))
