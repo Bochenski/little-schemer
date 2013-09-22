55c3eaee11490333aaa8c056bdb425ad1b814b8a
David Bochenski <david@bochenski.co.uk>
2013-09-22 08:54:25 +0100

    we improve rember to use equal in C5

--- definitions.scm
+++ definitions.scm
@@ -19,12 +19,6 @@
 
 
 ; Chapter 3
-(define rember
-  (lambda (a lat)
-    (cond
-      ((null? lat) (quote ()))
-      ((eq? (car lat) a) (cdr lat))
-      (else (cons (car lat) (rember a (cdr lat)))))))
 
 (define firsts
   (lambda (l)
