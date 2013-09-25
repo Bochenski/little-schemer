8bdb216fe3a67a879be8aef643afe15a95e914fb
David Bochenski <david@bochenski.co.uk>
2013-09-25 16:48:14 +0100

    Chapter 7 completed

--- definitions.scm
+++ definitions.scm
@@ -330,3 +330,109 @@
         (o* (value (1st-sub-exp nexp)) (value (2nd-sub-exp nexp))))
       (else
         (o^ (value (1st-sub-exp nexp)) (value (2nd-sub-exp nexp)))))))
+
+(define set?
+  (lambda (l) 
+    (cond 
+      ((null? l) #t)
+      ((member? (car l) (cdr l)) #f)
+      (else (set? (cdr l))))))
+
+(define makeset
+  (lambda (lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else (cons (car lat) 
+        (makeset 
+          (multirember (car lat) (cdr lat))))))))
+
+(define subset?
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) #t)
+      (else 
+        (and (member? (car set1) set2) (subset? (cdr set1) set2))))))
+
+(define eqset?
+  (lambda (set1 set2)
+    (and (subset? set1 set2) (subset? set2 set1))))
+
+(define intersect?
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) #f)
+      (else 
+        (or (member? (car set1) set2) (intersect? (cdr set1) set2))))))
+
+(define intersect
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) (quote ()))
+      ((member? (car set1) set2) (cons (car set1) (intersect (cdr set1) set2)))
+      (else (intersect (cdr set1) set2)))))
+
+
+(define union
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) set2)
+      ((member? (car set1) set2) (union (cdr set1) set2))
+      (else (cons (car set1) (union (cdr set1) set2))))))
+
+(define relative-complement
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) (quote ()))
+      ((member? (car set1) set2) (relative-complement (cdr set1) set2))
+      (else (cons (car set1) (relative-complement (cdr set1) set2))))))
+
+(define intersectall
+  (lambda (l-set)
+    (cond
+      ((null? (cdr l-set)) (car l-set))
+      (else  (intersect (car l-set) (intersectall (cdr l-set)))))))
+
+(define a-pair?
+  (lambda (x) 
+    (cond
+      ((atom? x) #f)
+      ((null? x) #f)
+      ((null? (cdr x)) #f)
+      ((null? (cdr (cdr x))) #t)
+      (else #f))))
+
+(define first
+  (lambda (p) 
+    (car p)))
+
+(define second
+  (lambda (p)
+    (car (cdr p))))
+
+(define third
+  (lambda (l)
+    (car (cdr (cdr l)))))
+
+(define build
+  (lambda (s1 s2)
+    (cons s1 (cons s2 (quote ())))))
+
+(define fun?
+  (lambda (rel)
+    (set? (firsts rel))))
+
+(define revpair
+  (lambda (pair)
+    (build (second pair) (first pair))))
+
+(define revrel
+  (lambda (rel)
+    (cond
+      ((null? rel) (quote ()))
+      (else (cons 
+        (revpair (car rel)) 
+        (revrel (cdr rel)))))))
+
+(define one-to-one?
+  (lambda (fun)
+    (fun? (revrel fun))))
